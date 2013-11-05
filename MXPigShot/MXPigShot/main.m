a562c238e66b64899d278df00fad0c965150da8c
longquan <longquan@taobao.com>
2013-11-05 17:16:56 +0800

    工程初始化

---
+++ MXPigShot/MXPigShot/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MXPigShot
+//
+//  Created by longquan on 13-11-5.
+//  Copyright longquan 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
